edb139e552e0131e45e57f0a72aabfeb86b34ddb
Felipe Seixas <seixasfelipe@gmail.com>
2013-02-17 08:18:21 -0500

    new basic rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CleanersPanamby::Application.config.secret_token = '6d45501f2850c7fb728cdc1d37b8d27de7674455b64e5d54eeae66152abf58fa38f998e259c641a43c32cd340c0153b8eddc5879defc0765c5b4695bf1862df2'
