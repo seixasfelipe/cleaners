d1ec820ff3596a170e72618a64dbb92f05a63262
Felipe Seixas <seixasfelipe@gmail.com>
2013-02-20 08:30:52 -0500

    unicorn listens to  environment variable

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -27,7 +27,7 @@ worker_processes 3
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
 #listen "/tmp/.sock", :backlog => 64
-listen 8000, :tcp_nopush => true
+listen ENV['PORT'], :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
