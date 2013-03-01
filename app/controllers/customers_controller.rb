2c09537c9a1474fdd9d5f5cd425e0eeb158b6c59
Felipe Seixas <seixasfelipe@gmail.com>
2013-03-01 00:02:50 -0500

    new customer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -7,4 +7,30 @@ class CustomersController < ApplicationController
       format.json { render :json => @customers }
     end
   end
+
+  def new
+    @customer = Customer.new
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @customer }
+    end
+  end
+
+  def create
+    @customer = Customer.new(params[:customer])
+ 
+    respond_to do |format|
+      if @customer.save
+        format.html  { redirect_to(customers_url,
+                      :notice => 'Cliente cadastrado com sucesso.') }
+        format.json  { render :json => @customer,
+                      :status => :created, :location => @customer }
+      else
+        format.html  { render :action => "new" }
+        format.json  { render :json => @customer.errors,
+                      :status => :unprocessable_entity }
+      end
+    end
+  end
 end
