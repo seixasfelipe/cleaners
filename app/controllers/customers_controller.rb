6f00726da0b2d130ce3bcf6f0b409c0dbae585e5
Felipe Seixas <seixasfelipe@gmail.com>
2013-03-01 15:21:23 -0500

    Fixes #5 - Use responders gem FTW

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,35 +2,19 @@ class CustomersController < ApplicationController
   def index
     @customers = Customer.all
 
-    respond_to do |format|
-      format.html
-      format.json { render :json => @customers }
-    end
+    respond_with @customers
   end
 
   def new
     @customer = Customer.new
 
-    respond_to do |format|
-      format.html
-      format.json { render :json => @customer }
-    end
+    respond_with @customer
   end
 
   def create
     @customer = Customer.new(params[:customer])
- 
-    respond_to do |format|
-      if @customer.save
-        format.html  { redirect_to(customers_url,
-                      :notice => 'Cliente cadastrado com sucesso.') }
-        format.json  { render :json => @customer,
-                      :status => :created, :location => @customer }
-      else
-        format.html  { render :action => "new" }
-        format.json  { render :json => @customer.errors,
-                      :status => :unprocessable_entity }
-      end
-    end
+    @customer.save
+
+    respond_with @customer
   end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,66 +2,39 @@ class TasksController < ApplicationController
   def index
   	@tasks = Task.all
 
-  	respond_to do |format|
-  		format.html
-  		format.json { render :json => @tasks }
-  	end
+  	respond_with @tasks
   end
 
   def new
   	@task = Task.new
 
-  	respond_to do |format|
-  		format.html
-  		format.json { render :json => @task }
-  	end
+  	respond_with @task
   end
 
   def create
   	@task = Task.new(params[:task])
- 
-  	respond_to do |format|
-  		if @task.save
-  		  format.html  { redirect_to(tasks_url,
-  		                :notice => 'Tarefa cadastrada com sucesso.') }
-  		  format.json  { render :json => @task,
-  		                :status => :created, :location => @task }
-  		else
-  		  format.html  { render :action => "new" }
-  		  format.json  { render :json => @task.errors,
-  		                :status => :unprocessable_entity }
-  		end
-  	end
+    @task.save
+
+    respond_with @task
   end
 
   def edit
     @task = Task.find(params[:id])
+
+    respond_with @task
   end
 
   def update
     @task = Task.find(params[:id])
+    @task.update_attributes(params[:task])
    
-    respond_to do |format|
-      if @task.update_attributes(params[:task])
-        format.html  { redirect_to(tasks_url,
-                      :notice => 'Tarefa atualizada com sucesso.') }
-        format.json  { head :no_content }
-      else
-        format.html  { render :action => "edit" }
-        format.json  { render :json => @task.errors,
-                      :status => :unprocessable_entity }
-      end
-    end
+    respond_with @task
   end
 
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
    
-    respond_to do |format|
-      format.html { redirect_to tasks_url,
-                    :notice => 'Tarefa "%s" removida com sucesso.' % @task.name }
-      format.json { head :no_content }
-    end
+    respond_with @task
   end
 end
