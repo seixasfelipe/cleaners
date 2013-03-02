6c85d27e9c1ecf7e9ddf8f054aa17d28627bf535
Fabiano Almeida <fabianoalmeida@gmail.com>
2013-03-01 21:37:28 -0300

    Login simple_form wrapper
    We could use this wrapper only to be used on login page.

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -37,6 +37,16 @@ SimpleForm.setup do |config|
     end
   end
 
+  config.wrappers :login, :class => 'clearfix', :error_class => 'error' do |b|
+    b.use :placeholder
+    b.use :label
+    b.wrapper :tag => 'div' do |ba|
+      ba.use :input
+      ba.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
+      ba.use :hint,  :wrap_with => { :tag => 'span', :class => 'help-block' }
+    end
+  end
+
   # Wrappers for forms and inputs using the Twitter Bootstrap toolkit.
   # Check the Bootstrap docs (http://twitter.github.com/bootstrap)
   # to learn about the different styles for forms and inputs,
