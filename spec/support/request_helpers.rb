c268cbf49c7a63e0baaad7cd7918e9767db85c1a
Felipe Seixas <seixasfelipe@gmail.com>
2013-03-05 23:37:45 -0500

    Fix some typo (accents, and translation) to make all tests become green.

--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -6,8 +6,8 @@ Warden.test_mode!
 def login(login, password)
   visit root_path
   within("#new_user") do
-    fill_in 'Login', with: login
-    fill_in 'Password', with: password
-    click_link_or_button 'Sign in'
+    fill_in 'Usuario', with: login
+    fill_in 'Senha', with: password
+    click_link_or_button 'Entrar'
   end
 end
