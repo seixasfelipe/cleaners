bc2d4ed6f241f451eb39e49036befbb234ac0cde
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-03-09 20:26:16 -0500

    fixing signing in spec, all test passes

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -6,14 +6,14 @@ feature "Signing in" do
     user = FactoryGirl.create(:user)
     login user.username, user.password
     expect(current_path).to eq root_path
-    expect(page).to have_content "Login efetuado com sucesso."
-    expect(page).to have_text "Atalhos"
+    expect(page).to have_content I18n.t('devise.sessions.signed_in')
+    expect(page).to have_text I18n.t('helpers.header.shortcuts')
   end
 
   scenario "with incorrect credentials" do
     login 'bla', 'blablabla'
     expect(current_path).to eq new_user_session_path
-    expect(page).to have_text("e-mail ou senha invalida")
+    expect(page).to have_text I18n.t('devise.failure.not_found_in_database')
   end
 
 end
