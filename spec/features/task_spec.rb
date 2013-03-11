624bd575ee3f1281aa4e14fa6832cf19ea70b1c3
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-03-11 01:09:01 -0400

    fix tasksSpec, all tests passes this fixes and #21 can be closed.

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -5,7 +5,7 @@ feature "Navigating on task views and" do
   background do
     user = FactoryGirl.create(:user)
     login user.username, user.password
-    expect(page).to have_content "Login efetuado com sucesso."
+    expect(page).to have_content I18n.t('devise.sessions.signed_in')
   end
 
   scenario "listing no results" do
@@ -20,8 +20,8 @@ feature "Navigating on task views and" do
   scenario "listing one result" do
     new_created_task
     page.has_selector?("table#tasks tbody tr", :count => 1)
-    expect(page).to have_link "Editar"
-    expect(page).to have_link "Remover"
+    expect(page).to have_link I18n.t('helpers.submit.edit')
+    expect(page).to have_link I18n.t('helpers.submit.remove')
   end
 
   scenario "listing five results" do
@@ -35,13 +35,13 @@ feature "Navigating on task views and" do
     new_created_task
 
     page.has_selector?("table#tasks tbody tr", :count => 1)
-    expect(page).to have_link "Editar"
+    expect(page).to have_link I18n.t('helpers.submit.edit')
 
-    click_link_or_button 'Editar'
+    click_link_or_button I18n.t('helpers.submit.edit')
 
-    expect(page).to have_content "Editar Tarefa"
+    expect(page).to have_content I18n.t('helpers.header.edit_model')
     fill_in task_form_name, with: 'Task Modified'
-    click_link_or_button 'Gravar'
+    click_link_or_button I18n.t('helpers.submit.save')
 
     page.has_selector?("table#tasks tbody tr", :count => 1)
     expect(page).to have_content "Task Modified"
@@ -51,11 +51,11 @@ feature "Navigating on task views and" do
     new_created_task
 
     page.has_selector?("table#tasks tbody tr", :count => 1)
-    expect(page).to have_link "Remover"
+    expect(page).to have_link I18n.t('helpers.submit.remove')
 
-    click_link_or_button 'Remover'
+    click_link_or_button I18n.t('helpers.submit.remove')
 
-    expect(page).to have_content "Tarefa removido(a) com sucesso."
+    expect(page).to have_content I18n.t('flash.actions.destroy.notice', :resource_name => I18n.t('activerecord.models.task'))
 
     page.has_selector?("table#tasks tbody tr", :count => 0)
   end
@@ -64,18 +64,18 @@ feature "Navigating on task views and" do
 
   def task_index_path
     click_link_or_button 'Listar Tarefas'
-    expect(page).to have_link "Inserir"
+    expect(page).to have_link I18n.t('helpers.submit.create')
   end
 
   def new_created_task
     task_index_path
-    click_link_or_button 'Inserir'
+    click_link_or_button I18n.t('helpers.submit.create')
     within("#new_task") do
       fill_in task_form_name, with: 'Task name'
       fill_in I18n.t("tasks.form.price"), with: 1.5
-      click_link_or_button 'Gravar'
+      click_link_or_button I18n.t('helpers.submit.save')
     end
-    expect(page).to have_content "Tarefa cadastrado(a) com sucesso."
+    expect(page).to have_content I18n.t('flash.actions.create.notice', :resource_name => I18n.t('activerecord.models.task'))
   end
 
   def task_form_name
