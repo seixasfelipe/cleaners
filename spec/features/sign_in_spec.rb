35a9deae6b8e034ba5ea52e980e318a9fc420a75
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-03-12 00:08:43 -0400

    we don't need to type anymore I18n.t on rspec tests, saves some types =)

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -6,14 +6,14 @@ feature "Signing in" do
     user = FactoryGirl.create(:user)
     login user.username, user.password
     expect(current_path).to eq root_path
-    expect(page).to have_content I18n.t('devise.sessions.signed_in')
-    expect(page).to have_text I18n.t('helpers.header.shortcuts')
+    expect(page).to have_content t('devise.sessions.signed_in')
+    expect(page).to have_text t('helpers.header.shortcuts')
   end
 
   scenario "with incorrect credentials" do
     login 'bla', 'blablabla'
     expect(current_path).to eq new_user_session_path
-    expect(page).to have_text I18n.t('devise.failure.not_found_in_database')
+    expect(page).to have_text t('devise.failure.not_found_in_database')
   end
 
 end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -4,7 +4,7 @@ feature "Signing up" do
 
   scenario "with valid params" do
     visit root_path
-    click_link I18n.t('helpers.links.create_user')
+    click_link t('helpers.links.create_user')
     expect(current_path).to eq new_user_registration_path
     within("#new_user") do
       fill_in 'First name', with: 'First name'
@@ -13,22 +13,22 @@ feature "Signing up" do
       fill_in 'Email', with: 'user@name.com'
       fill_in 'Password', with: '12345678'
       fill_in 'Password confirmation', with: '12345678'
-      click_link_or_button I18n.t('helpers.links.create_user')
+      click_link_or_button t('helpers.links.create_user')
     end
     expect(current_path).to eq root_path
-    expect(page).to have_content I18n.t('devise.registrations.signed_up')
-    expect(page).to have_text I18n.t('helpers.header.shortcuts')
+    expect(page).to have_content t('devise.registrations.signed_up')
+    expect(page).to have_text t('helpers.header.shortcuts')
   end
 
   scenario "with invalid params" do
     visit root_path
-    click_link I18n.t('helpers.links.create_user')
+    click_link t('helpers.links.create_user')
     expect(current_path).to eq new_user_registration_path
     within("#new_user") do
-      click_link_or_button I18n.t('helpers.links.create_user')
+      click_link_or_button t('helpers.links.create_user')
     end
     expect(current_path).to eq user_registration_path
-    expect(page).to have_content I18n.t('simple_form.error_notification.default_message')
+    expect(page).to have_content t('simple_form.error_notification.default_message')
   end
 
 end
