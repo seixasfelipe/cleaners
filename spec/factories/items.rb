6a0ba82ecb0df0486f1c433e36c048e0f8cd49c2
Fabiano Almeida <fabianoalmeida@gmail.com>
2013-03-12 09:06:31 -0300

    Renaming Task model name to Item
    It's related to #15 when after a discussion the participants defined that the Task model should be called Item.

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :task do
+  factory :item do
     name { FactoryGirl.generate :name }
     price 1.5
     active true
