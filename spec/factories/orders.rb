f4eeda182eda11085e4225ecc09259b4ddc5a055
Felipe Seixas <seixasfelipe@gmail.com>
2013-04-10 08:13:53 -0400

    adds order model

---
+++ spec/factories/orders.rb
@@ -0,0 +1,11 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :order do
+    closed false
+    order_date DateTime.new
+    value 50.0
+    vehicle { FactoryGirl.build(:vehicle) }
+    vehicle_id 5
+  end
+end
