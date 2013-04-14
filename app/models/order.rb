f59b45d6408a09b6e99227ba4e8f4828fc81cb9a
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-04-14 19:33:06 -0400

    fixing wrong presence and attributes, should be vehicle_id

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ActiveRecord::Base
-  attr_accessible :order_date, :value, :closed, :vehicle
+  attr_accessible :order_date, :value, :closed, :vehicle_id
 
-  validates_presence_of :order_date, :vehicle
+  validates_presence_of :order_date, :vehicle_id
 
   validates_inclusion_of :closed, :in => [true, false]
 
