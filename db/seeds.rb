b6d9b72d72bed4386772a7c6eff4a268e66a5730
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-04-27 11:18:44 -0400

    changing to 2 letters code #48

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-countries = Country.create([{name: 'Canada', abbreviation:'CAN'},
-                            {name: 'Brazil', abbreviation: 'BRA'},
-                            {name: 'United States', abbreviation: 'USA'}])
+countries = Country.create([{name: 'Canada', abbreviation:'CA'},
+                            {name: 'Brazil', abbreviation: 'BR'},
+                            {name: 'United States', abbreviation: 'US'}])
